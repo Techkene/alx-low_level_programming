94adb409eba845218cb8a26310ea7173328b6359
Techkene <keneanoliefo24@gmail.com>
2022-12-09 10:57:04 -0800

    C program that prints exactly...... using Put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0 (sucess)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+} 
