c65d6827e2aa11e9c27891a8e8d5b716e0b2382d
Techkene <keneanoliefo24@gmail.com>
2022-12-11 07:26:34 -0800

    Final task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - prints exactly and that piece of art is useful
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 61);
+	return (1);
+}
