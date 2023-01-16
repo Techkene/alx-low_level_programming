482e9b4bec1e3d86a1d253b76b3c970d42008f0c
Techkene <keneanoliefo24@gmail.com>
2023-01-16 15:04:36 -0800

    summit solution

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,9 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdlib>
+/**
+  * _strlen - returns length of a string
+  * @str: string to be counted
+  * Return: returns length of string
+  */
+int _strlen(char *str)
